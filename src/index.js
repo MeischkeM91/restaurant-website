7eb3999bafd6d23bfef07896aa053393820c2acd
Max Meischke <meischkem@outlook.com>
2022-04-19 13:28:22 -0400

    Created homepage and styled

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './style.css';
+import Bartender from './bartender.jpeg';
 
+
+const hero = document.querySelector('.hero');
+// Image of bartender
+const bartender = new Image();
+bartender.src = Bartender;
+bartender.classList.add('bartender')
+
+hero.appendChild(bartender);
 
 //alert('Everything is working!');

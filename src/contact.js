f4e5eb430285b3618b0148ce43c13436d82e452e
Max Meischke <meischkem@outlook.com>
2022-04-20 11:13:27 -0400

    Styled contact form

--- src/contact.js
+++ src/contact.js
@@ -16,21 +16,19 @@ const createContact = () =>{
     navbar.classList.add('navbar');
     // Navbar links
     const homeLink = document.createElement('div');
-    homeLink.classList.add('navlink','home-link','active');
+    homeLink.classList.add('navlink','home-link');
     homeLink.innerText = 'Home';
     homeLink.addEventListener('click', () =>{
         loadHome();
-        console.log('clicked')
     });
     const menuLink = document.createElement('div');
     menuLink.classList.add('navlink','menu-link');
     menuLink.innerText = 'Menu';
     const contactLink = document.createElement('div');
-    contactLink.classList.add('navlink','contact-link');
+    contactLink.classList.add('navlink','contact-link','active');
     contactLink.innerText = 'Contact';
     contactLink.addEventListener('click', () =>{
         loadContact();
-        console.log('clicked')
     });
     // Add top bar to page
     pageWrapper.appendChild(pageLogo);
@@ -56,18 +54,21 @@ const createContact = () =>{
     // User details portion
     const contactDetails = document.createElement('div');
     contactDetails.classList.add('contactDetails');
+    const nameDiv = document.createElement('div');
     const nameLabel = document.createElement('label');
     nameLabel.for='nameField';
     nameLabel.innerText='Name: ';
     const nameField = document.createElement('input');
     nameField.type='text';
     nameField.name='nameField';
+    const phoneDiv = document.createElement('div');
     const phoneLabel = document.createElement('label');
     phoneLabel.for='phoneField';
     phoneLabel.innerText='Phone: ';
     const phoneField = document.createElement('input');
     phoneField.type='text';
     phoneField.name='phoneField';
+    const emailDiv = document.createElement('div');
     const emailLabel = document.createElement('label');
     emailLabel.for='emailField';
     emailLabel.innerText='Email: ';
@@ -78,7 +79,12 @@ const createContact = () =>{
     const textareaHolder = document.createElement('div');
     const textareaField = document.createElement('textarea');
     textareaField.style.width='100%';
-    textareaField.rows='10';
+    textareaField.rows='7';
+    // Submit button
+    const subBtn = document.createElement('button');
+    subBtn.type='button';
+    subBtn.classList.add('subBtn');
+    subBtn.innerText = 'Submit';
 
     //Create form
     pageContent.appendChild(contactForm);
@@ -86,13 +92,17 @@ const createContact = () =>{
     formFieldset.appendChild(formLegend);
     formFieldset.appendChild(contactDetails);
     formFieldset.appendChild(textareaHolder);
-    contactDetails.appendChild(nameLabel);
-    contactDetails.appendChild(nameField);
-    contactDetails.appendChild(phoneLabel);
-    contactDetails.appendChild(phoneField);
-    contactDetails.appendChild(emailLabel);
-    contactDetails.appendChild(emailField);
+    contactDetails.appendChild(nameDiv);
+    nameDiv.appendChild(nameLabel);
+    nameDiv.appendChild(nameField);
+    contactDetails.appendChild(phoneDiv);
+    phoneDiv.appendChild(phoneLabel);
+    phoneDiv.appendChild(phoneField);
+    contactDetails.appendChild(emailDiv);
+    emailDiv.appendChild(emailLabel);
+    emailDiv.appendChild(emailField);
     textareaHolder.appendChild(textareaField);
+    formFieldset.appendChild(subBtn);
 };
 
 export default function loadContact() {

16100da7ce0b90797f11233d39ede05e3903dd14
Max Meischke <meischkem@outlook.com>
2022-04-20 12:48:11 -0400

    Added Business details and styled

--- src/contact.js
+++ src/contact.js
@@ -43,8 +43,25 @@ const createContact = () =>{
     const pageContent = document.createElement('div');
     pageContent.classList.add('contact-page-content');
     pageWrapper.appendChild(pageContent);
+    /***** Details Form ******/
+    const genDetails = document.createElement('div');
+    genDetails.classList.add('gen-details');
+    const addressDetails = document.createElement('p');
+    addressDetails.classList.add('address-details');
+    addressDetails.innerHTML = `Visit Us @<br>W 52nd St & 6th Ave,<br> New York, NY 10019`;
+    const hoursDetails = document.createElement('p');
+    hoursDetails.classList.add('hours-details');
+    hoursDetails.innerHTML = '<span class="hours">Hours</span><br><br>SUN: 12p - 2a<br>MON: 4p - 2a<br>TUE: 4p - 2a<br>WED: 4p - 2a<br>THUR: 4p - 2a<br>FRI: 4p - 4a<br>SAT: 12p - 4a<br>';
+    const mapDetails = document.createElement('iframe');
+    mapDetails.classList.add('map-details');
+    mapDetails.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3332.894086610008!2d-73.98185314895976!3d40.76128054243458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25965167d9c77%3A0x93a5b0d26a4071fa!2sGalbi%20Q!5e1!3m2!1sen!2sus!4v1650379074781!5m2!1sen!2sus";
+    // Create Details Container
+    pageContent.appendChild(genDetails);
+    genDetails.appendChild(addressDetails);
+    genDetails.appendChild(hoursDetails);
+    genDetails.appendChild(mapDetails); 
 
-    // Contact Form
+    /***** Contact Form *****/
     const contactForm = document.createElement('form');
     contactForm.action='post';
     const formFieldset = document.createElement('fieldset');
@@ -92,7 +109,6 @@ const createContact = () =>{
         textareaField.value='';
         alert('Thank you for submitting your feedback!');
     });
-
     //Create form
     pageContent.appendChild(contactForm);
     contactForm.appendChild(formFieldset);
